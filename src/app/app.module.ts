a254f83b0a0c9c79f341692956035f09ea57da58
xcshanto <shanto906l@gmail.com>
2018-05-05 14:27:28 +0600

    form added with no validation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,14 @@
+import { SignupFormComponent } from './signup-form/signup-form.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, 
+    SignupFormComponent
   ],
   imports: [
     BrowserModule
