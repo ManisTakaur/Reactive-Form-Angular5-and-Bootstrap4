6ec3f471d286c129dbed896e9ba4a86c8a52050f
xcshanto <shanto906l@gmail.com>
2018-05-05 23:29:43 +0600

    user validation added

--- src/app/signup-form/signup-form.component.ts
+++ src/app/signup-form/signup-form.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'signup-form',
@@ -8,9 +8,16 @@ import { FormGroup, FormControl } from '@angular/forms';
 })
 export class SignupFormComponent {
 
-  formcontrol= new FormGroup({
-    username: new FormControl(),
-    password: new FormControl()
-  })
+  formcontrol= new FormGroup({  //instance of a form group object
+
+    //instance of password and username has been created
+    username: new FormControl('', Validators.required), //formcontrol is derived from abstract class
+    password: new FormControl('', Validators.required) 
+    //validators added in form control objects and initially set to null  
+  });
+
+  get username(){
+    return this.formcontrol.get('username'); //to get the value of username in div
+  }
 
 }
